44c36a0ae1ea4921e101099280d6f47dbe9a1d8b
Daniel Plazas <danieljpc19@gmail.com>
2023-10-02 21:07:39 -0500

    upload templates (List interface and Node abstract class)

---
+++ src/model/templates/List.java
@@ -0,0 +1,9 @@
+package model.templates;
+
+public interface List<T extends Comparable<T>>{
+    public T get();
+    public boolean add(T t);
+    public T remove();
+
+    public boolean isEmpty();
+}
---
+++ src/model/templates/Node.java
@@ -0,0 +1,27 @@
+package model.templates;
+
+public abstract class Node<T extends Comparable<T>>{
+    private T t;
+
+    private Node<T> next;
+
+    public Node(T t){
+        this.t = t;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
